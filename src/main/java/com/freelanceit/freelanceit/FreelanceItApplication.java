04f0aed485cc8015d668b6a1addcb73ea18aee5e
Vinay Thamara <thamarvd@mail.uc.edu>
2024-10-17 15:11:05 -0400

    Project Setup

---
+++ src/main/java/com/freelanceit/freelanceit/FreelanceItApplication.java
@@ -0,0 +1,13 @@
+package com.freelanceit.freelanceit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelanceItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelanceItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/freelanceit/freelanceit/FreelanceItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freelanceit.freelanceit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelanceItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
