acbc115f89a63e343d0f90c33dd71ad18e6775ab
Vinay Thamara <thamarvd@mail.uc.edu>
2024-11-05 01:00:49 -0500

    Link Service to DAO

--- src/main/java/service/ProjectService.java
+++ src/main/java/service/ProjectService.java
@@ -17,21 +17,21 @@ public class ProjectService implements IProjectService {
 
     @Override
     public Project fetchById(int id) {
-        return null;
+        return projectDAO.fetch(id);
     }
 
     @Override
     public List<Project> fetchAll() {
-        return List.of();
+        return projectDAO.findAll();
     }
 
     @Override
     public void delete(int id) throws Exception {
-
+        projectDAO.delete(id);
     }
 
     @Override
     public Project save(Project project) throws Exception {
-        return null;
+        return projectDAO.save(project);
     }
 }
