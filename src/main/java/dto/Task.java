918b6531efc79bf19422f1db4d8889cafce15aa7
Somyani Ghimire <112344638+ghimirsi@users.noreply.github.com>
2024-11-06 03:27:58 -0500

    connecting to database

---
+++ src/main/java/dto/Task.java
@@ -0,0 +1,14 @@
+package com.freelanceit.freelanceit.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Task {
+    // Getters and setters
+    private String title;
+    private String description;
+    private String assigned;
+
+}
