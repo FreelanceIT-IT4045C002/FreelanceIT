68bec0333b77879ade0ed2f3d8e9b4b02d90dd44
Vinay Thamara <thamarvd@mail.uc.edu>
2024-11-29 16:50:39 -0500

    Define User Model

---
+++ src/main/java/com/freelanceit/freelanceit/dto/User.java
@@ -0,0 +1,35 @@
+package com.freelanceit.freelanceit.dto;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String email;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+}
