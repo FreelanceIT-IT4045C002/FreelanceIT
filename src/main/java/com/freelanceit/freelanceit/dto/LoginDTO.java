1912a28d4445216d1efc7b62c7af1486f7511d88
Jasmine Lim Smith <jasminelimsmith30@gmail.com>
2024-12-03 17:34:50 -0500

    add javadoc for LoginDTO

--- src/main/java/com/freelanceit/freelanceit/dto/LoginDTO.java
+++ src/main/java/com/freelanceit/freelanceit/dto/LoginDTO.java
@@ -3,9 +3,22 @@ package com.freelanceit.freelanceit.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Data Transfer Object (DTO) for login credentials.
+ * This class encapsulates the username and password used for authentication.
+ * It uses Lombok annotations to automatically generate getter and setter methods.
+ */
 @Getter
 @Setter
 public class LoginDTO {
+
+    /**
+     * The username for authentication.
+     */
     private String username;
+
+    /**
+     * The password for authentication.
+     */
     private String password;
 }
