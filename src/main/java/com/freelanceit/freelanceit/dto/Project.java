3cee8b4333a95ea2c4390c0637f8066a1ef7ff8d
Jasmine Lim Smith <jasminelimsmith30@gmail.com>
2024-12-03 17:36:47 -0500

    Add javadoc for Project DTO

--- src/main/java/com/freelanceit/freelanceit/dto/Project.java
+++ src/main/java/com/freelanceit/freelanceit/dto/Project.java
@@ -4,20 +4,51 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represents a Project entity in the system.
+ * This class is mapped to a database table using JPA annotations.
+ * It uses Lombok's @Data annotation to automatically generate boilerplate code.
+ */
 @Entity
-public @Data class Project {
+@Data
+public class Project {
+
+    /**
+     * The unique identifier for the project.
+     * This field is automatically generated.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int projectId;
 
+    /**
+     * The user associated with this project.
+     * This establishes a Many-to-One relationship with the User entity.
+     */
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    /**
+     * The title of the project.
+     * This field cannot be null.
+     */
     @Column(nullable = false)
     private String Title;
+
+    /**
+     * The start date of the project.
+     */
     private LocalDateTime StartDate;
+
+    /**
+     * The deadline for the project completion.
+     */
     private LocalDateTime Deadline;
+
+    /**
+     * The actual end date of the project.
+     */
     private LocalDateTime EndDate;
 }
 
