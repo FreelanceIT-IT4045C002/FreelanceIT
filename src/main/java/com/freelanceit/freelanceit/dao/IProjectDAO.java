12454258c4e72dd269889d66826df49232fda2ba
Jasmine Lim Smith <jasminelimsmith30@gmail.com>
2024-12-04 15:22:33 -0500

    Added Javadoc for IProjectDAO

--- src/main/java/com/freelanceit/freelanceit/dao/IProjectDAO.java
+++ src/main/java/com/freelanceit/freelanceit/dao/IProjectDAO.java
@@ -4,9 +4,40 @@ import com.freelanceit.freelanceit.dto.Project;
 
 import java.util.List;
 
+/**
+ * Interface for Data Access Object (DAO) that provides methods
+ * for managing Project entities in a persistent storage.
+ */
 public interface IProjectDAO {
+
+    /**
+     * Saves a given project to the database.
+     *
+     * @param project the project to be saved
+     * @return the saved project, including any generated fields (e.g., ID)
+     * @throws Exception if there is an error during saving
+     */
     Project save(Project project) throws Exception;
+
+    /**
+     * Retrieves all projects from the database.
+     *
+     * @return a list of all projects
+     */
     List<Project> findAll();
+
+    /**
+     * Fetches a project by its unique identifier.
+     *
+     * @param id the unique identifier of the project
+     * @return the project with the specified id, or null if not found
+     */
     Project fetch(int id);
+
+    /**
+     * Deletes a project by its unique identifier.
+     *
+     * @param id the unique identifier of the project to be deleted
+     */
     void delete(int id);
 }
