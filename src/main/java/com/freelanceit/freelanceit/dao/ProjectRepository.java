62d4163cc010b9ee886084aba19b59822bb087a5
Jasmine Lim Smith <jasminelimsmith30@gmail.com>
2024-12-04 15:32:15 -0500

    Added Javadoc for ProjectRepository

--- src/main/java/com/freelanceit/freelanceit/dao/ProjectRepository.java
+++ src/main/java/com/freelanceit/freelanceit/dao/ProjectRepository.java
@@ -3,4 +3,12 @@ package com.freelanceit.freelanceit.dao;
 import com.freelanceit.freelanceit.dto.Project;
 import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Repository interface for managing Project entities.
+ *
+ * This interface extends CrudRepository to provide CRUD operations
+ * for Project entities with Integer as the ID type.
+ */
 public interface ProjectRepository extends CrudRepository<Project, Integer> { }
