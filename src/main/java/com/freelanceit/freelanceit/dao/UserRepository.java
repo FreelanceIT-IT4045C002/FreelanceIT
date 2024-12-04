9d3c62d641bdd043921212512520c8edbc3ea3b3
Jasmine Lim Smith <jasminelimsmith30@gmail.com>
2024-12-04 15:37:49 -0500

    Added Javadoc for UserRepository

--- src/main/java/com/freelanceit/freelanceit/dao/UserRepository.java
+++ src/main/java/com/freelanceit/freelanceit/dao/UserRepository.java
@@ -3,6 +3,21 @@ package com.freelanceit.freelanceit.dao;
 import com.freelanceit.freelanceit.dto.User;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Repository interface for managing User entities.
+ *
+ * This interface extends CrudRepository to provide CRUD operations
+ * for User entities with Integer as the ID type. It also includes
+ * a method to find a user by their username.
+ */
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+    /**
+     * Retrieves a user by their username.
+     *
+     * @param username the username of the user to be retrieved
+     * @return the User entity associated with the specified username,
+     *         or null if no user is found
+     */
     User findByUsername(String username);
 }
